4ee42aa6b3c7612078fce67d98ade9d882b02066
Andreas Botsikas <1227433+andreasbotsikas@users.noreply.github.com>
2021-03-09 01:10:14 +0200

    Reverse order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while :; do 
-   sleep 12h & wait $!;
    certbot certonly -d $DomainName -a dns-azure --dns-azure-credentials /var/azure-dns-cred.json --dns-azure-resource-group $AzureDnsResourceGroup --renew-by-default --text --email $ContactEmail --agree-tos -q; nginx -s reload; 
+   sleep 12h & wait $!;
 done & nginx -g 'daemon off;'
